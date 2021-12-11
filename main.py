65b73a994fdc95d7dd065d1569a3661684c59037
TomasHricina <tomas.hricina314@gmail.com>
2021-12-11 15:42:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ import pandas as pd
 from pykrakenapi import KrakenAPI
 import krakenex
 from forex_python.converter import CurrencyRates
+import os
 
 
 cost_treshold = 0.1
 just_default = 14
 round_default = 4
 
-df = pd.read_csv(r'trades.csv')
+df = pd.read_csv(os.path.join(os.path.split(__file__)[0], 'trades.csv'))
 keep_cols = ['time', 'pair', 'type', 'cost', 'fee', 'vol']
 df = df[keep_cols]
 euro_to_czk = CurrencyRates().get_rate('EUR', 'CZK')
-
 
 def notation(col: pd.Series) -> list:
     return [float('%f' % x) for x in col.tolist()]
@@ -35,7 +35,7 @@ df = df.drop([x[0] for x in enumerate(vol) if x[1] < cost_treshold])
 df.reset_index(drop=True, inplace=True)
 
 coins = list(set(df['pair'].to_list()))
-coin_dfs = {coin: [] for coin in coins}
+coin_dfs = {coin:[] for coin in coins}
 
 for index, row in df.iterrows():
     coin_dfs[row['pair']].append(row)
@@ -43,7 +43,6 @@ for index, row in df.iterrows():
 for k, v in coin_dfs.items():
     coin_dfs[k] = pd.DataFrame(v)
     coin_dfs[k].reset_index(drop=True, inplace=True)
-
 
 def coin_analysis(coin):
     print()
@@ -63,20 +62,20 @@ def coin_analysis(coin):
         sell = -1 if typ == 'sell' else 1
         eur = row['cost'] * -sell
         tokens = row['vol'] * sell
-        avg = -eur / tokens
+        avg = -eur/tokens
         tot_eur += eur
         tot_tokens += tokens
-        if tot_tokens <= cost_treshold:  # stake
+        if tot_tokens <= cost_treshold: # stake
             tot_tokens = 0
             tot_avg = 0
         else:
-            tot_avg = abs(tot_eur / tot_tokens)
-
+            tot_avg = abs(tot_eur/tot_tokens)
+        
         print(eval(_eval))
-
+    
     if tot_avg <= cost_treshold:
         profit = round(tot_eur, round_default)
-        print('You exited with:', profit, ' Euro')
+        print('You exited with:', profit, ' Euro' )
     else:
         print('Your average is:', round(tot_avg, round_default), ' Euro/token')
         current_price = get_price(coin)
@@ -85,9 +84,8 @@ def coin_analysis(coin):
         print('Your profit is: ', profit, ' Euro')
     return profit
 
-
 total_profit = 0
 for coin in coin_dfs.keys():
     total_profit += coin_analysis(coin)
-
-print('\nYour total profit is:  ', total_profit, 'Euro', ' or ', total_profit * euro_to_czk, ' CZK')
+    
+print('\nYour total profit is:  ', total_profit, 'Euro', ' or ', total_profit*euro_to_czk, ' CZK')
